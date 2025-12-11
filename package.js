6f140186e7d05621fa0026a519fe3c7e9f5cd7ac
jimmy443dd <member.chime@yandex.com>
2025-12-12 00:01:54 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "email-extractor",
+  "version": "1.0.0",
+  "description": "Simple email extractor web app.",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "node-fetch": "^3.3.1"
+  }
+}
